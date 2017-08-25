ddaa5bed1b32ca3e08fd9d9176a528eaaa9c43bd
szaske <github@zaske.com>
2017-08-25 14:01:55 -0700

    add ability to add new units to the roster

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Unit } from '../unit';
+import { UnitService } from '../unit.service';
+
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
+  providers: [UnitService]
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private unitService: UnitService) { }
 
   ngOnInit() {
   }
 
+  submitForm(name: string, mineral: number, vespene: number, desc: string) {
+    var newUnit: Unit = new Unit(name,  
+      mineral, 
+      vespene,
+      1,
+      1,
+      "buildFrom",
+      23,
+      23,
+      1,
+      desc);
+    this.unitService.addUnit(newUnit);
+  }
 }
+
+// name,  
+// mineral, 
+// vespene,
+// supply,
+// buildTime,
+// buildFrom, 
+// life,
+// energy,
+// armor,
+// desc
